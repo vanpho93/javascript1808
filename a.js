44ba9ca880c09e88d0742efdb7bea1a81ec215fe
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-21 19:43:39 +0700

    xing

---
+++ a.js
@@ -0,0 +1,13 @@
+const a = 5;
+const b = 10;
+
+if (a > b) {
+    console.log(a);
+} else {
+    console.log(b);
+}
+
+const max = a > b ? a : b;
+console.log(max);
+
+a > b ? console.log(a) : console.log(b);
